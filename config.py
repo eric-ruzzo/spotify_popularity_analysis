db89c39b27df6daed8099378ed6290d89528b40f
Ruzzo <eric.ruzzo@gmail.com>
2019-04-11 23:00:53 -0400

    finalized API calls

---
+++ config.py
@@ -0,0 +1,3 @@
+client_id = "c81893462e33426ba51d5755d2e976c3"
+secret = "b282106d9df441eea9c95696410519ea"
+api_key = "BQCydlGKpLrt5FXsjuMsU13r8eihVzKmOyaXgJiygJDJxYgCgNo7xFpSvDAjHPOQarfOY0xYaheayP43Gy-QJ19IUojVIOw0qAHz1-5zOmIww-HBaAp2Zig4hBl7Ww44efLjDypYHRJ2c-4"
